258a96df3692af9047f6a5afb74f816c0f70d467
nmc.net <kysutudongcom@gmail.com>
2024-11-14 17:50:27 +0700

    init

---
+++ basic_app/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 26, 2, 80),
+              Color.fromARGB(255, 45, 7, 98)
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello World!',
+              style: TextStyle(fontSize: 30, color: Colors.white),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
